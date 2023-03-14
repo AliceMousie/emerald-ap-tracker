edae9064221781a4ed18504cce38c6126a6dce3e
Alice <alice@rootsys.sh>
2023-03-14 16:34:42 +0000

    Dungeon maps, Autotracking fixes

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,17 +2,19 @@ Tracker:AddItems("items/items.json")
 -- Logic
 ScriptHost:LoadScript("scripts/logic/logic.lua")
 
-if not IS_ITEMS_ONLY then
-    -- Maps
-    Tracker:AddMaps("maps/maps.json")    
-    -- Locations
-    Tracker:AddLocations("locations/locations.json")
+-- Maps
+Tracker:AddMaps("maps/maps.json")    
+-- Locations
+Tracker:AddLocations("locations/locations.json")
+if PopVersion and PopVersion >= "0.23.0" then
+    Tracker:AddLocations("locations/dungeons.json")
 end
 
 -- Layout
 Tracker:AddLayouts("layouts/events.json")
 Tracker:AddLayouts("layouts/settings.json")
 Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/tabs.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
 
