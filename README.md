8abfb9b4ab60fd3826750818c4709b86a0c98783
Alice <alice@rootsys.sh>
2023-03-16 13:36:55 +0000

    a

---
+++ README.md
@@ -0,0 +1,3 @@
+# Emerald AP Tracker
+
+PopTracker pack for Archipelago Pokémon Emerald with Autotracking
---
+++ scripts/init.lua
@@ -0,0 +1,24 @@
+Tracker:AddItems("items/items.json")
+-- Logic
+ScriptHost:LoadScript("scripts/logic/logic.lua")
+
+-- Maps
+Tracker:AddMaps("maps/maps.json")    
+-- Locations
+Tracker:AddLocations("locations/locations.json")
+if PopVersion and PopVersion >= "0.23.0" then
+    Tracker:AddLocations("locations/dungeons.json")
+end
+
+-- Layout
+Tracker:AddLayouts("layouts/events.json")
+Tracker:AddLayouts("layouts/settings.json")
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/tabs.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+
+-- AutoTracking for Poptracker
+if PopVersion and PopVersion >= "0.18.0" then
+    ScriptHost:LoadScript("scripts/autotracking.lua")
+end
