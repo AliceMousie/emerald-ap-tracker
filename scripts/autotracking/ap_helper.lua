bf8e87c4be18bbeded1c763fef2db19e4b5d1af4
アリス <alice@rootsys.sh>
2023-11-10 10:43:31 +0000

    Add goal setting

--- scripts/autotracking/ap_helper.lua
+++ scripts/autotracking/ap_helper.lua
@@ -22,6 +22,7 @@ end
 
 MAP_TOGGLE={[0]=0,[1]=1}
 MAP_TOGGLE_REVERSE={[0]=1,[1]=0}
+MAP_TRIPLE={[0]=0,[1]=1,[2]=2}
 MAP_TRIPLE_REVERSE={[0]=2,[1]=1,[2]=0}
 MAP_FREEFLY={[0]=0,[5]=1,[6]=2,[7]=3,[8]=4,[9]=5,[10]=6,[11]=7,[12]=8,[13]=9,[15]=10}
 MAP_BADGES={}
@@ -99,6 +100,10 @@ SLOT_CODES = {
 	elite_four_count={
 		code="e4req", 
 		mapping=MAP_BADGES
+	},
+	goal={
+		code="goal", 
+		mapping=MAP_TRIPLE
 	}
 }
 
